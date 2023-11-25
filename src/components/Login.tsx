228c5f708a14f6a2a6340bd8f0c4ed177966baa6
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2023-11-25 19:53:08 +0500

    update login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -45,7 +45,7 @@ const Login = () => {
     <div className="flex flex-col items-center mt-52">
       <p className="text-center text-xl mb-4">Please login to Play.</p>
       <button onClick={logIn} className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600">Login/Signup</button>
-      <p className="text-center text-red-500 font-medium text-xl my-3">In case of ay error. PLease clear cache an reload.</p>
+      <p className="text-center text-red-500 font-medium text-xl my-3">In case of any error. Please clear cache and reload.</p>
     </div>
   );
 };
